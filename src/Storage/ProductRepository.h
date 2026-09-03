e0c6ee3dcd6d494a454be9d327c59ade052d3bdc
Bakos Benjamin <bakos.benjamin.zoltan@gmail.com>
2026-09-03 14:16:04 +0200

    decided against separate header-cpp so merged them

--- src/Storage/ProductRepository.h
+++ src/Storage/ProductRepository.h
@@ -1,17 +1,51 @@
+#pragma once
+
 #include "../Objects/Product.h"
 #include "TsvParser.h"
+#include <stdexcept>
+#include <string>
 #include <string_view>
 #include <vector>
 
 class ProductRepository {
 public:
   static constexpr std::string_view FILENAME = "products.tsv";
-  void load();
-  void save();
-  void upsert();
-  Product find(int id);
+
+  void load() {
+    std::string filename = std::string(TsvParser::DATA_FOLDER) +
+                           std::string(ProductRepository::FILENAME);
+    std::vector<std::vector<std::string>> parsedData =
+        TsvParser::parse(filename);
+
+    for (size_t i = 1; i < parsedData.size(); ++i) {
+      products.push_back(hydrate(parsedData[i]));
+    }
+  }
+
+  Product find(int id) {
+    if (products.empty()) {
+      throw std::runtime_error("products are empty");
+    }
+
+    for (const auto &product : products) {
+      if (product.id == id) {
+        return product;
+      }
+    }
+
+    throw std::runtime_error("product couldn't be found");
+  }
 
 private:
   std::vector<Product> products;
-  Product hydrate(std::vector<std::string>);
+
+  Product hydrate(std::vector<std::string> row) {
+    Product product;
+    product.id = std::stoi(row.at(0));
+    product.name = row.at(1);
+    product.description = row.at(2);
+    product.image = "";
+
+    return product;
+  }
 };
--- src/Storage/TsvParser.h
+++ src/Storage/TsvParser.h
@@ -1,3 +1,7 @@
+#pragma once
+
+#include <fstream>
+#include <sstream>
 #include <string>
 #include <string_view>
 #include <vector>
@@ -5,6 +9,27 @@
 class TsvParser {
 public:
   static constexpr std::string_view DATA_FOLDER = "data/";
-  static std::vector<std::vector<std::string>> parse(std::string filename);
-  static std::vector<std::string> splitLine(const std::string &line);
+
+  static std::vector<std::vector<std::string>> parse(std::string filename) {
+    std::vector<std::vector<std::string>> rows;
+    std::ifstream filestream(filename);
+    std::string line;
+
+    while (std::getline(filestream, line)) {
+      rows.push_back(splitLine(line));
+    }
+
+    return rows;
+  }
+
+  static std::vector<std::string> splitLine(const std::string &line) {
+    std::vector<std::string> fields;
+    std::string field;
+    std::istringstream stream(line);
+    while (std::getline(stream, field, '\t')) {
+      fields.push_back(field);
+    }
+
+    return fields;
+  }
 };
