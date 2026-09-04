f05eb9d503fcbf2c77a96cd6aa0f2480db36a571
Bakos Benjamin <bakos.benjamin.zoltan@gmail.com>
2026-09-04 08:38:09 +0200

    trying to style

--- src/Storage/ShoppingListRepository.h
+++ src/Storage/ShoppingListRepository.h
@@ -6,9 +6,11 @@
 #include <string_view>
 #include <unordered_map>
 #include <vector>
+
 class ShoppingListRepository {
 public:
   static constexpr std::string_view FILENAME = "shopping_lists.tsv";
+
   void load() {
     std::string filename =
         std::string(TsvParser::DATA_FOLDER) + std::string(FILENAME);
@@ -18,12 +20,7 @@ public:
         TsvParser::parse(filename);
 
     for (int i = 1; parsedData.size(); i++) {
-      auto &row = parsedData[i];
-      int listId = std::stoi(row.at(0));
-      ShoppingList &shoppingList = byId[listId];
-      ListItem listItem = listItemRepository.find(std::stoi(row.at(1)));
-
-      shoppingList.listItems.push_back(listItem);
+      hydrate(byId, parsedData, i);
     }
 
     for (auto &[id, loadedList] : byId) {
@@ -35,5 +32,16 @@ public:
 
 private:
   std::vector<ShoppingList> shoppingLists;
+
   ListItemRepository listItemRepository;
+
+  void hydrate(std::unordered_map<int, ShoppingList> &byId,
+               std::vector<std::vector<std::string>> &parsedData, int &i) {
+    auto &row = parsedData[i];
+    int listId = std::stoi(row.at(0));
+    ShoppingList &shoppingList = byId[listId];
+    ListItem listItem = listItemRepository.find(std::stoi(row.at(1)));
+
+    shoppingList.listItems.push_back(listItem);
+  }
 };
